2ab2c0a00be07df5ab4e23bb0ee1ab294b9c4045
Aadhil Negim <88481193+mitnichiter@users.noreply.github.com>
2025-10-07 00:19:01 +0530

    Update page.tsx

--- carmel-pdf-generator/src/app/page.tsx
+++ carmel-pdf-generator/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="text-4xl font-bold text-center">Carmel PDF Generator</h1>
+        <h1 className="text-4xl font-bold text-center">Report Generator</h1>
       </div>
       <div className="mt-10 w-full max-w-lg">
         <ReportForm />
